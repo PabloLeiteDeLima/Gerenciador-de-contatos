25c208a863a4a4bda1af0412783ea3d3f62f216f
Pablo Leite <pabloleitelima7@gmail.com>
2026-08-17 22:50:32 -0300

    adicionando documentação na index.php

--- index.php
+++ index.php
@@ -1,22 +1,32 @@
 <?php
+/*
+* @author: Pablo Leite
+* version: 1.0
+* date: 17-08-2026. 
+*/ 
 
+// anexando a pagina classContato.php
 require_once "classContato.php";
 
+// instanciando o objeto.
 $contato1 = new Contato("Pablo", "pablo@gmail.com", 992920536);
+
+// mostrando o retorno de todos os dados.
 echo $contato1->exibirDados();
 
 echo "<p>Testando Gets...<br>";
-echo $contato1->getNome();
-echo "<br>";
-echo $contato1->getEmail();
-echo "<br>";
-echo $contato1->getTelefone();
-echo "<br>";
+echo $contato1->getNome() . "<br>";
+echo $contato1->getEmail() . "<br>";
+echo $contato1->getTelefone() . "<br>";
 
+// setando novos valores...
 $contato1->setNome("Pablo Leite");
 $contato1->setEmail("Paaabloooo@gmail.com");
 $contato1->setTelefone(99991212);
+
 echo "<br>";
+
+// mostrando o retorno de todos os dados atualizados...
 echo $contato1->exibirDados();
 
 
